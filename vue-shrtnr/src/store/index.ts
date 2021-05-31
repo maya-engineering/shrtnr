693b8e035e4121cb157d83e597d7809df37ca854
Mostak Ahmad <mostakahmademo@gmail.com>
2021-05-31 19:58:22 +0600

    frontend vue app

---
+++ vue-shrtnr/src/store/index.ts
@@ -0,0 +1,20 @@
+import {Commit, createStore} from 'vuex'
+import auth from './auth'
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+
+export default new Vuex.Store({ 
+    state: {
+        //
+    },
+    mutations: {
+        //
+    },
+    actions: {
+        //
+    },
+    modules: {
+      auth
+    }
+})
